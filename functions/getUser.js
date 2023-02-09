6e926853f66852470398e97ad74edbc2fee18f6b
masontmorris <masonmorris8@gmail.com>
2023-02-09 03:38:33 -0500

    fixed sample twitch axios request

--- functions/getUser.js
+++ functions/getUser.js
@@ -1,24 +1,21 @@
-const dotenv = require('dotenv')
-const axios = require('axios');
+const dotenv = require("dotenv");
+const axios = require("axios");
 dotenv.config();
 
 (async () => {
     try {
         const config = {
             headers: {
-                'Authorization': 'Bearer qtayz84fmjo6ql0oqxotejlfbwo7rl',
-                'Client-Id': process.env.TWITCH_CLIENT_ID,
-                "Content-Type": "application/json"
-                
-            }
-            
-        }
+                Authorization: "Bearer " + process.env.TWITCH_ACCESS_TOKEN,
+                "Client-Id": process.env.TWITCH_CLIENT_ID,
+                "Content-Type": "application/json",
+            },
+        };
 
-    const response = await axios.get('https://api.twitch.tv/helix/users?login=silkyyy_johnson', config).then(function(response) {
-        console.log(response)
-
-    })
+        const response = await axios.get("https://api.twitch.tv/helix/users?login=silkyyy_johnson", config).then(function (response) {
+            console.log(response.data.data[0].id);
+        });
     } catch (error) {
-        console.log('error ' + error);
+        console.log("error " + error);
     }
 })();
